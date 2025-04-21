fdea88286f708871883712067292947a08039ef4
Angina <203669321+Iamangina@users.noreply.github.com>
2025-04-21 10:37:32 +0000

    First commit

---
+++ script.js
@@ -0,0 +1,85 @@
+let choice = ["rock", "paper", "scissors"];
+let ComputerChoice;
+let UserChoice;
+let userScore = 0; 
+let computerScore = 0;  
+let roundCount = 0;  
+
+const score = document.querySelectorAll('.score');
+const playerScore = score[0];  
+const computerScoreElement = score[1];  
+
+const result = document.querySelector('.result'); 
+
+function getComputerChoice() {
+    ComputerChoice = Math.random();
+    ComputerChoice = ComputerChoice * choice.length;
+    ComputerChoice = Math.floor(ComputerChoice);
+    ComputerChoice = choice[ComputerChoice];
+    console.log(`Computer chose: ${ComputerChoice}`);  
+    return ComputerChoice;
+}
+
+function getUserChoice(e) {
+    UserChoice = e.currentTarget.getAttribute('data-choice');  
+    console.log(`User chose: ${UserChoice}`);  
+}
+
+
+const rock = document.querySelector('.rock');
+rock.addEventListener('click', (e) => {
+    getUserChoice(e);
+    ComputerChoice = getComputerChoice();
+    playRound();
+});
+const paper = document.querySelector('.paper');
+paper.addEventListener('click', (e) => {
+    getUserChoice(e);
+    ComputerChoice = getComputerChoice();
+    playRound();
+});
+
+const scissors = document.querySelector('.scissors');
+scissors.addEventListener('click', (e) => {
+    getUserChoice(e);
+    ComputerChoice = getComputerChoice();
+    playRound();
+});
+
+function playRound() {
+    console.log(`User choice: ${UserChoice}, Computer choice: ${ComputerChoice}`);  
+
+    if (UserChoice === ComputerChoice) {
+        result.textContent = "DRAW";
+    } else if (
+        (UserChoice === "rock" && ComputerChoice === "scissors") ||
+        (UserChoice === "paper" && ComputerChoice === "rock") ||
+        (UserChoice === "scissors" && ComputerChoice === "paper")
+    ) {
+        result.textContent = "YOU WON";
+        userScore++;  
+    } else {
+        result.textContent = "YOU LOST";
+        computerScore++;  
+    }
+
+    playerScore.textContent = `SCORE: ${userScore}`;
+    computerScoreElement.textContent = `SCORE: ${computerScore}`;
+    roundCount++;
+
+
+    if (roundCount === 5) {
+        
+        if (userScore > computerScore) {
+            result.textContent = "YOU WIN THE GAME!";
+        } else if (userScore < computerScore) {
+            result.textContent = "YOU LOSE THE GAME!";
+        } else {
+            result.textContent = "THE GAME IS A DRAW!";
+        }
+
+        rock.disabled = true;
+        paper.disabled = true;
+        scissors.disabled = true;
+    }
+}
